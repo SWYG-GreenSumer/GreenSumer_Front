11a434fe64374686b2ab83d15250847ff9399e36
Lee-SoYoon <99dlthdbs@gmail.com>
2022-11-13 17:24:44 +0900

    Update 💄 Navigation CSS 수정
    1. Title 글씨 크기 크게
    2. 로그인 버튼 예쁘게
    3. 카테고리별 여백 추가

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,13 +4,13 @@ const Nav = () => {
   return (
     <div className='bg-[#1C541B] text-white flex justify-between'>
       <div className='flex items-center ml-10'>
-        <p>GreenSumer!</p>
+        <p className='text-3xl'>GreenSumer!</p>
       </div>
-      <div className='mr-10'>        
-        <div className='flex justify-end'>
-          <button className='bg-white text-black'>로그인</button>
+      <div className='mr-10 mt-6'>        
+        <div className='flex justify-end mb-3'>
+          <button className='bg-white text-black w-40 h-9 rounded-md'>로그인</button>
         </div>
-        <div className='flex'>
+        <div className='flex mb-3'>
           <div className='ml-4'>매장운영(Seller)</div>
           <div className='ml-4'>제품 검색</div>
           <div className='ml-4'>그린카드</div>
