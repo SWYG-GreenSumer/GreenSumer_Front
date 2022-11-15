2da9897e430881300f69b86ba04e9c42989f52d1
Lee-SoYoon <99dlthdbs@gmail.com>
2022-11-15 19:53:09 +0900

    Update :art: 로그인 페이지 반응형으로 구현

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
+interface LoginProps {}
 
-const Login = () => {
+const Login: FunctionComponent<LoginProps> = () => {
+  const onLoginHandler = (e: React.MouseEvent) => {
+    e.preventDefault();
+    return null;
+  }
   return (
-    <div>
+    <div className='LoginContainer mb-10'>
+      <div className='Logo flex justify-center m-10'>
+        <p className='font-bold italic text-secondary text-4xl'>
+          Welcome to <br /> GreenSumer!
+        </p>
+      </div>
+      <div className='formContainer'>
+        <form method='post' className='w-100'>
+          <div className='m-auto form-control w-full max-w-xs'>
+            <label className='label'>
+              <span className='label-text'>ID</span>
+            </label>
+            <input
+              id='userID'
+              type='text'
+              placeholder='ID를 입력하세요'
+              className=' input input-bordered w-full max-w-xs'
+            />
+          </div>
+          <div className='m-auto form-control w-full max-w-xs'>
+            <label className='label'>
+              <span className='label-text'>Password</span>
+            </label>
+            <input
+              id='userPW'
+              type='text'
+              placeholder='비밀번호를 입력하세요'
+              className='input input-bordered w-full max-w-xs'
+            />
+          </div>
+          <div className='max-w-xs m-auto mt-8'>
+            <button className='btn btn-block' onClick={onLoginHandler}>Enter!</button>
+          </div>
+        </form>
+      </div>
+      <div className='findIDandSignupContainer'>
+        <div className='flex flex-col'>
+          <Link to='/findIDandPW' className='mt-4 text-neutral m-auto text-sm'>
+            아이디/비밀번호 찾기
+          </Link>
+          <Link to='/signup' className='mt-4 text-neutral m-auto text-sm'>
+            회원가입이 아직이라면 클릭하세요!
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
+{
+  /* <div>
       <div className='flex justify-center'>
         <p className='font-bold italic text-[#18BC3C] mt-20 text-7xl'>
           Welcome to <br />
@@ -46,11 +104,12 @@ const Login = () => {
         </form>
       </div>
       <div className='flex flex-col'>
-        <button className='mt-4 text-[#0A0D0A] m-auto'>아이디/비밀번호 찾기</button>
-        <button className='mt-10 mb-10 text-[#0A0D0A] m-auto'>회원가입이 아직이라면 클릭하세요!</button>
+        <button className='mt-4 text-[#0A0D0A] m-auto'>
+          아이디/비밀번호 찾기
+        </button>
+        <button className='mt-10 mb-10 text-[#0A0D0A] m-auto'>
+          회원가입이 아직이라면 클릭하세요!
+        </button>
       </div>
-    </div>
-  );
-};
-
-export default Login;
+    </div> */
+}
