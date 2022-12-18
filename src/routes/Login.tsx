dca79cb471d2abc4742bbd04919db2f551655590
Lee-SoYoon <99dlthdbs@gmail.com>
2022-12-19 01:46:56 +0900

    Update 🎨 api 파라미터 값 변경

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -15,7 +15,7 @@ const Login: FunctionComponent<LoginProps> = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          userid: userID,
+          username: userID,
           password: userPW,
         }),
       };
