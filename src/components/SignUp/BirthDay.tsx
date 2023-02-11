1a55ed1b113b74267863b68543d3d5ccc19904de
Lee-SoYoon <99dlthdbs@gmail.com>
2023-02-11 23:38:44 +0900

    feat ✨ add func that choose BirthDate  in SignUp

---
+++ src/components/SignUp/BirthDay.tsx
@@ -0,0 +1,60 @@
+import React, { FunctionComponent, useCallback } from 'react';
+
+/**
+ * 년, 월에 따라 마지막 날짜가 무엇인지 계산하는 함수
+ *
+ * example :
+ *
+ * let days = getDaysInMonth(2, 2022);
+ *
+ * console.log(days); // 28
+ *
+ * @param month number
+ * @param year number
+ * @returns last day
+ *
+
+ * 
+ */
+const getDaysInMonth = (month: string, year: string) => {
+  return new Date(+year, +month, 0).getDate();
+};
+
+type BirthDayProps = {
+  birthMonth: string;
+  birthYear: string;
+  setUserBirthDay: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const BirthDay: FunctionComponent<BirthDayProps> = ({
+  birthMonth,
+  birthYear,
+  setUserBirthDay,
+}) => {
+  const birthDayLength = getDaysInMonth(birthMonth, birthYear);
+  const birthDayArray = Array.from(
+    { length: birthDayLength },
+    (_, index) => index + 1
+  );
+
+  const onClickBirthDayHandler = useCallback((event: React.MouseEvent<HTMLElement>): void => {
+    const currDay = event.currentTarget.textContent;
+    if(currDay !== null){
+        setUserBirthDay(currDay);
+    }
+  },[])
+
+  return (
+    <div>
+      {birthDayArray.map((e) => {
+        return (
+          <li key={e}>
+            <a onClick={onClickBirthDayHandler}>{e}</a>
+          </li>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BirthDay;
---
+++ src/components/SignUp/BirthYear.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent, useCallback } from 'react';
+
+type BirthYearProps = {
+  setUserBirthYear: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const BirthYear: FunctionComponent<BirthYearProps> = ({ setUserBirthYear }) => {
+  const birthYear = new Date().getFullYear();
+  const birthYearArray = Array.from({ length: 104 }, (_, index) =>
+    (birthYear - index).toString()
+  );
+
+  const onClickBirthYearHandler = useCallback(
+    (event: React.MouseEvent<HTMLElement>): void => {
+      const currYear = event.currentTarget.textContent;
+      if (currYear !== null) {
+        setUserBirthYear(currYear);
+      }
+    },
+    []
+  );
+
+  return (
+    <div>
+      {birthYearArray.map((e) => {
+        return (
+          <li key={e}>
+            <a onClick={onClickBirthYearHandler}>{e}</a>
+          </li>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BirthYear;
