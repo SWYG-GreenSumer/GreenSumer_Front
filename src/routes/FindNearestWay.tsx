ece6dd3e10bfb70a5f8afb15eb105a212b4f255c
Lee-SoYoon <99dlthdbs@gmail.com>
2023-02-14 00:12:47 +0900

    chore: add StoreDrawer in findNearestWay

--- src/routes/FindNearestWay.tsx
+++ src/routes/FindNearestWay.tsx
@@ -1,15 +1,32 @@
-import React, { FunctionComponent, useEffect,useState, useRef } from 'react';
+import { FunctionComponent } from 'react';
 import StoreDrawer from '../components/KakaoMap/StoreDrawer';
-import { MapMarker, Map, useMap } from 'react-kakao-maps-sdk';
 import KakaoMap from '../components/KakaoMap/KakaoMap';
 
 type FindNearestWayProps = {};
 
 const FindNearestWay: FunctionComponent<FindNearestWayProps> = () => {
+  const data = [
+    {
+      content: <div style={{ color: '#000' }}>강남역</div>,
+      latlng: { lat: 37.498095, lng: 127.02761 },
+    },
+    {
+      content: <div style={{ color: '#000' }}>서울시청</div>,
+      latlng: { lat: 37.541, lng: 126.986 },
+    },
+    {
+      content: <div style={{ color: '#000' }}>역삼역</div>,
+      latlng: { lat: 37.503325874722, lng: 127.04403462366 },
+    },
+    {
+      content: <div style={{ color: '#000' }}>선릉역</div>,
+      latlng: { lat: 37.503085510654, lng: 127.048359211 },
+    },
+  ];
   return (
     <div>
       <KakaoMap width={'100%'} height={'97vh'} />
-      {/* <StoreDrawer data={data} /> */}
+      <StoreDrawer data={data} />
     </div>
   );
 };
