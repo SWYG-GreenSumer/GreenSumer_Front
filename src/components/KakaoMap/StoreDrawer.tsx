0ff22dddc01d2cb7a68c71f3ab6c063ff78d1f92
Lee-SoYoon <99dlthdbs@gmail.com>
2023-02-14 00:14:05 +0900

    chore: add  data type

--- src/components/KakaoMap/StoreDrawer.tsx
+++ src/components/KakaoMap/StoreDrawer.tsx
@@ -1,34 +1,34 @@
 import React, { FunctionComponent } from 'react';
+import { storeDataJSON } from '../../../public/json/storeDataJSON';
+
+type dataArrayProps = {
+  content: JSX.Element;
+  latlng: {
+    lat: number;
+    lng: number;
+  };
+};
 
 type StoreDrawerProps = {
-  data: any;
+  data: dataArrayProps[];
 };
 
 const StoreDrawer: FunctionComponent<StoreDrawerProps> = ({ data }) => {
   return (
-    <div className='absolute right-0 top-0'>
-      <div className='drawer drawer-mobile'>
-        <input id='my-drawer-2' type='checkbox' className='drawer-toggle' />
-        <div className='drawer-content flex flex-col items-center justify-center'>
-          {/* <!-- Page content here --> */}
-          <label
-            htmlFor='my-drawer-2'
-            className='btn btn-primary drawer-button lg:hidden'>
-            Open drawer
-          </label>
-        </div>
+    <div className='absolute right-0 top-0 z-[1]'>
+      <div className='drawer drawer-mobile'>        
         <div className='drawer-side'>
           <label htmlFor='my-drawer-2' className='drawer-overlay'></label>
           <ul className='menu p-4 w-80 bg-base-100 text-base-content'>
             {/* <!-- Sidebar content here --> */}
-            {data?.map(
+            {data.map(
               (
                 e: {
-                  content: any;                 
+                  content: any;
                 },
                 index: React.Key | null | undefined
               ) => {
-                console.log(e)
+                console.log(e);
                 return (
                   <li key={index}>
                     <a className='text-black'>{e.content.props.children}</a>
