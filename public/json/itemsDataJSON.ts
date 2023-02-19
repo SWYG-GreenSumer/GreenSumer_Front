01ec22eabb0eb4d128d96b5d37a2aaf758e7649b
Lee-SoYoon <99dlthdbs@gmail.com>
2023-02-19 18:53:31 +0900

    chore: add itemsDataJSON file

---
+++ public/json/itemsDataJSON.ts
@@ -0,0 +1,35 @@
+// 제품이름, 제품 가격, 재고, 제품 설명, 이미지들
+export const itemsDataJSON = [
+  {
+    id: 0,
+    itemsName: '수세미',
+    price: '2000',
+    stock: 20,
+    description: '거품이 잘나는 최고급 수세미!',
+    images: ['0', '1', '2'],
+  },
+    {
+    id: 1,
+    itemsName: '주방 세제',
+    price: '3000',
+    stock: 10,
+    description: '지구를 아낄 수 있는 천연 주방 세제 입니다',
+    images: ['0', '1', '2'],
+  },
+    {
+    id: 2,
+    itemsName: '샴푸',
+    price: '4000',
+    stock: 30,
+    description: '두피를 깨끗하게 만들어주는 샴푸 입니다',
+    images: ['0', '1', '2'],
+  },
+    {
+    id: 3,
+    itemsName: '린스',
+    price: '5000',
+    stock: 60,
+    description: '머리카락을 물미역으로 만들어주는 린스 입니다',
+    images: ['0', '1', '2'],
+  },
+];
