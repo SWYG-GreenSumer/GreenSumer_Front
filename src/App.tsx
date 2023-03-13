86413a26ce25aefdab1691843a2f95870dde6c1d
Lee-SoYoon <99dlthdbs@gmail.com>
2023-03-13 21:06:35 +0900

    feat ✨ add authSatate in axios header
    axios header에 전역적으로 token을 넣어서 사용할 수 있도록 추가하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import AppRouter from './components/AppRouter';
 import { GlobalStyle } from './global-style';
 import { FunctionComponent } from 'react';
 import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import { authState } from './atoms/AuthTokensAtoms';
 
 declare global {
   interface Window {
@@ -11,11 +13,13 @@ declare global {
 
 type AppProps = {};
 
-axios.defaults.baseURL = 'https://port-0-greensumer-back-3xcah2glbj4mak4.gksl2.cloudtype.app';
+const App: FunctionComponent<AppProps> = () => {
+  const tokens = useRecoilValue(authState);
 
-axios.defaults.withCredentials = true;
-
-const App: FunctionComponent<AppProps> = () => {  
+  axios.defaults.baseURL =
+    'https://port-0-greensumer-back-3xcah2glbj4mak4.gksl2.cloudtype.app';
+  axios.defaults.withCredentials = true;
+  // axios.defaults.headers.common['Authorization'] = `Bearer ${tokens.accessToken}`;
   return (
     <div className='h-full bg-[#FDFDFD] text-black'>
       {/* 여기에 글로벌-스타일-컴포넌트 위치 */}
