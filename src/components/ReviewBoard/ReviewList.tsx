1910d00995fa8db075f4c41a10218e67e151571f
LAPTOP-GR2A5CGU\이소윤 <99dlthdbs@gmail.com>
2023-03-15 17:42:02 +0900

    feat ✨ add accessToken and refreshToken in Storage

--- src/components/ReviewBoard/ReviewList.tsx
+++ src/components/ReviewBoard/ReviewList.tsx
@@ -1,6 +1,9 @@
+import axios from 'axios';
 import React, { FunctionComponent, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 import { dumpData } from '../../../public/json/dumpdata';
+import { reviewPostsState } from './reviewPostsAtoms';
 
 type ReviewListProps = {};
 
@@ -8,17 +11,27 @@ const ReviewList: FunctionComponent<ReviewListProps> = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      const data = await fetch('/api/articles/news', {
-        method: 'GET',
-      });
-      const jsonData = await data.json();
-      console.log(jsonData);
-      //   setResult(jsonData.results);
-    };
+  const [reviewPosts, setReviewPosts] = useRecoilState(reviewPostsState);
 
-    fetchNews();
+  useEffect(() => {
+    axios
+      .get('/api/reviews/news/')
+      .then(
+        ({
+          data: {
+            resultCode,
+            result: { content },
+          },
+        }) => {
+          if (resultCode === 'SUCCESS') {
+            console.log(content);
+            setReviewPosts(content);
+          }
+        },
+      )
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
@@ -30,18 +43,17 @@ const ReviewList: FunctionComponent<ReviewListProps> = () => {
             {/* 후기 게시판 페이지에서만 표시 */}
             {location.pathname.includes('reviewBoard') && (
               <tr>
-                <th className="bg-success">번호</th>
-                <th className="bg-success">종류</th>
                 <th className="bg-success">제목</th>
                 <th className="bg-success">글쓴이</th>
-                <th className="bg-success">날짜</th>
+                <th className="bg-success">별점</th>
                 <th className="bg-success">조회수</th>
+                <th className="bg-success">좋아요 수</th>                
               </tr>
             )}
           </thead>
           <tbody>
             {/* <!-- row --> */}
-            {dumpData.map((e, i) => {
+            {reviewPosts.map((e, i) => {
               // 후기 게시판 페이지에서는 10개씩, 메인 페이지에서는 4개씩 보여주기
               if (location.pathname.includes('reviewBoard') && i < 10) {
                 if (i < 10) {
@@ -51,11 +63,11 @@ const ReviewList: FunctionComponent<ReviewListProps> = () => {
                       className="cursor-pointer"
                       onClick={() => navigate(`/reviewDetail/${e.product_id}`)}
                     >
-                      <th>{e.product_id}</th>
                       <td>{e.title}</td>
-                      <td>{e.nickname}</td>
-                      <td>{e.createdAt}</td>
+                      <td>{e.nickname}</td>                      
+                      <td>{e.scope}</td>
                       <td>{e.views}</td>
+                      <td>{e.likes}</td>
                     </tr>
                   );
                 }
